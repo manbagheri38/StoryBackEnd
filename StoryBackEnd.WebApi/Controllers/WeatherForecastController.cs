0cd467fa3618b260c03c716fcb2b1699a5e417cf
mansour <manbagheri38@gmail.com>
2022-06-09 16:06:52 +0430

    add rainy weather

--- StoryBackEnd.WebApi/Controllers/WeatherForecastController.cs
+++ StoryBackEnd.WebApi/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace StoryBackEnd.WebApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","rainy"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
