62b20d47e6ee56eaf1a05f76a97ecf52be80dfc8
mansour <manbagheri38@gmail.com>
2022-06-09 16:19:40 +0430

    add wares controller

---
+++ StoryBackEnd.WebApi/Controllers/WaresController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoryBackEnd.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WaresController : ControllerBase
+    {
+        public  IActionResult getAllWare()
+        {
+            var list = new List<string>
+            {
+                "book",
+                "pencil",
+                "robot"
+            };
+
+            return Ok(new { list = list });
+        }
+    }
+}
